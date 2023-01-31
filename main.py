c5e89c07d27f5a8ae10bf28fdd8a3245fff9e4c1
Dayo Greats <dayogreats@gmail.com>
2023-01-30 22:20:11 -0500

    sms features added

--- main.py
+++ main.py
@@ -3,26 +3,60 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 # Download the helper library from https://www.twilio.com/docs/python/install
+# Download the helper library from https://www.twilio.com/docs/python/install
 import os
 from twilio.rest import Client
+
 
 # Set environment variables for your credentials
 # Read more at http://twil.io/secure
 
-account_sid = "AC50007a7d454705d5a5888cf5ea73f159"
-auth_token = os.environ["TWILIO_AUTH_TOKEN"]
-client = Client(account_sid, auth_token)
 
-call = client.calls.create(
-  url="http://demo.twilio.com/docs/voice.xml",
-  to="+16463098171",
-  from_="+18556720674"
-)
+# VOICE OPTION
+class TwilioCall:
+
+    def __init__(self):
+        self.account_sid = "AC50007a7d454705d5a5888cf5ea73f159"
+        self.auth_token = "0660b64e38fd480ffbaf1f529c6b4eb9"
+        self.client = Client(self.account_sid, self.auth_token)
+
+    def caller(self):
+        call = self.client.calls.create(
+            url="http://demo.twilio.com/docs/voice.xml",
+            to="+16463098171",
+            from_="+18556720674"
+        )
+
+        return call.sid
+
+
+# SMS OPTION
+
+class TwilioSMS:
+    def __init__(self, account_sid, auth_token, from_number, to_number):
+        self.client = Client(account_sid, auth_token)
+        self.from_number = from_number
+        self.to_number = to_number
+
+    def send_message(self, message):
+        message = self.client.messages.create(
+            from_=self.from_number,
+            to=self.to_number,
+            body=message
+        )
+        return message.sid
 
 
 def main():
     # Use a breakpoint in the code line below to debug your script.
-    print(call.sid)
+    # Make a call
+    call = TwilioCall()
+    call.caller()
+
+    # Send Sms
+    sms = TwilioSMS('AC50007a7d454705d5a5888cf5ea73f159', '0660b64e38fd480ffbaf1f529c6b4eb9', '+18556720674',
+                    '+16463098171')
+    sms.send_message('HelLo Bisi, Bayo ni o. Ku ojo ematu. Oko e ati awon omo e nko. Bami kiwon o')
 
 
 # Press the green button in the gutter to run the script.

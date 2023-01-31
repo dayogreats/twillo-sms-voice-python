4f90686fcd35cca4406465ad1db1d4c3afb129d1
Dayo Greats <dayogreats@gmail.com>
2023-01-30 21:01:03 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+# Set environment variables for your credentials
+# Read more at http://twil.io/secure
+
+account_sid = "AC50007a7d454705d5a5888cf5ea73f159"
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+client = Client(account_sid, auth_token)
+
+call = client.calls.create(
+  url="http://demo.twilio.com/docs/voice.xml",
+  to="+16463098171",
+  from_="+18556720674"
+)
+
+
+def main():
+    # Use a breakpoint in the code line below to debug your script.
+    print(call.sid)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
